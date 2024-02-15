bd1ddce605895f64895b704d7dc1c186067abe01
NicasioMunyi <nickalfred200@gmail.com>
2024-02-15 10:19:36 +0300

    intial commit

---
+++ src/main/java/com/alfredoLibrary/alfredoLibrary/AlfredoLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.alfredoLibrary.alfredoLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlfredoLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlfredoLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alfredoLibrary/alfredoLibrary/AlfredoLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alfredoLibrary.alfredoLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlfredoLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
