7688b6b7790bb2f3ba3ab6f02d8857f4b65781b1
NicasioMunyi <nickalfred200@gmail.com>
2024-02-18 19:48:06 +0300

    security Set up

---
+++ src/main/java/com/alfredoLibrary/alfredoLibrary/configurations/SecurityConfig.java
@@ -0,0 +1,10 @@
+package com.alfredoLibrary.alfredoLibrary.configurations;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+}
